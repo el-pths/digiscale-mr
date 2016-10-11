163418e572cb1bd6b78453eb8eaf0d171d3953a6
RakickijMihail <mixrak@mail.ru>
2016-10-11 16:29:49 +0000

    добавлен функционал установки нуля

--- digiscale.ino
+++ digiscale.ino
@@ -1,18 +1,58 @@
+template<size_t Size> double getArithmeticMean(uint32_t (&values)[Size]);
 
 const int LED_PIN = 16;
 const int SCK_PIN = 15;
 const int DT_PIN = 14;
+const int TARE_PIN = 5;
+double adjustment = 0;
 
 void setup() {
   Serial.begin(9600);
   pinMode(LED_PIN, OUTPUT);
   pinMode(SCK_PIN, OUTPUT);
   pinMode(DT_PIN, INPUT);
+  pinMode(TARE_PIN, INPUT_PULLUP);
 }
 
 void loop() {
-  Serial.println(((float)getScaledValue() - 59600)/217);
+  if(digitalRead(TARE_PIN) == LOW){
+    digitalWrite(LED_PIN, HIGH);
+    setAdjustment();
+    digitalWrite(LED_PIN, LOW);
+  }
+  uint32_t arr[10];
+  for(int i = 0; i<10; i++){
+    arr[i] = getScaledValue();
+    delay(10);
+  }
+  Serial.println((getArithmeticMean(arr) - adjustment)/217);
   delay(1000);
+}
+
+void setAdjustment(){
+  uint32_t arr[10];
+  for(int i = 0; i<10; i++){
+    arr[i] = getScaledValue();
+    delay(10);
+  }
+  adjustment = getArithmeticMean(arr);
+}
+
+template<size_t Size> double getArithmeticMean(uint32_t (&values)[Size]){
+  double s = 0;
+  for(int i = 0; i<Size; i++){
+    s += values[i];
+  }
+  return s/Size;
+}
+
+template<size_t Size> double getStandardDeviation(uint32_t (&values)[Size]){
+  double s = 0;
+  double mean = getArithmeticMean(values);
+  for(int i = 0; i<Size; i++){
+    s += sq(values[i] - mean);
+  }
+  return sqrt(s/Size);
 }
 
 uint32_t getScaledValue(){
